53cfa90d0ea6ad89f3dc2eb3209d5e0c0f6a2946
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-12 15:34:56 +0600

    chore: enable JavaScript support in TypeScript configuration
    - Added "allowJs": true to the tsconfig.json to permit JavaScript files in the TypeScript project, enhancing compatibility with mixed codebases.

---
+++ src/__tests__/examples/example1.js
@@ -0,0 +1,67 @@
+/* eslint-disable */
+
+const app = na.createApp('App', {
+  description: 'App description',
+
+  arguments: [
+    na.argument('name', String, {
+      description: 'A string argument',
+    }),
+  ],
+
+  options: [
+    na.option('name', String, {
+      description: 'A string option',
+    }),
+
+    na.option('age', Number, {
+      description: 'A number option with multiple values',
+    }),
+
+    na.option('tags', [String], {
+      description: 'A string option with multiple values',
+    }),
+
+    na.option('isAdmin', Boolean, {
+      description: 'A boolean option',
+    }),
+
+    na.option('verbose', Boolean, {
+      description: 'Verbose output',
+    }),
+  ],
+
+  handle({ args, options }) {
+    console.log('App handle')
+    args.name // The first argument
+
+    options.name // The string option
+    options.age // The number option
+    options.tags // The string option with multiple values
+    options.isAdmin // The boolean option
+    options.verbose // The boolean option
+  },
+})
+
+const childApp = na.create('ChildApp', {
+  description: 'ChildApp description',
+
+  arguments: [
+    na.argument('name', String, {
+      description: 'A string argument',
+    }),
+  ],
+
+  options: [
+    na.option('string', String, {
+      description: 'A string option',
+    }),
+  ],
+
+  handle({ args, options }) {
+    console.log('ChildApp handle')
+  },
+})
+
+app.start()
+app.start(['-v', '1', '2', '3'])
